abcc26b5a9123ab5f8049c2c811f681b476d9ed8
mperkins1995 <mperkins1995@gmail.com>
2023-05-12 11:29:58 -0500

    completed mobile first responsive nav

--- script.js
+++
@@ -1,5 +0,0 @@
-const navbar = document.querySelector(".navbar");
-const menuToggle = document.querySelector(".menu-toggle");
-menuToggle.addEventListener("click", () => {
-  navbar.classList.toggle("open");
-});
