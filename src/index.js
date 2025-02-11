b5785994794209139a8541c16fe6469c06c7c511
Michael Perkins <69735071+michaperki@users.noreply.github.com>
2025-02-11 02:54:57 -0600

    Update routing to use hash router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename="/connect4">
+  <HashRouter basename="/connect4">
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
+
